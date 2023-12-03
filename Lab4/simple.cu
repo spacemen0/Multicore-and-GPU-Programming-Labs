a5873033eceba56ad46d226f91a556f2f10eb911
Spacemen0 <frakctures@outlook.com>
2023-12-03 22:23:19 +0100

    modify simple

--- Lab4/simple.cu
+++ Lab4/simple.cu
@@ -5,34 +5,54 @@
 // nvcc simple.cu -L /usr/local/cuda/lib -lcudart -o simple
 
 #include <stdio.h>
+#include <math.h>
 
-const int N = 16; 
-const int blocksize = 16; 
+const int N = 16;
+const int blocksize = 16;
 
-__global__ 
-void simple(float *c) 
+__global__ void simple(float *c)
 {
 	c[threadIdx.x] = threadIdx.x;
+}
+__global__ void ssqrt(float *i)
+{
+	i[threadIdx.x] = sqrt(threadIdx.x);
 }
 
 int main()
 {
-	float *c = new float[N];	
-	float *cd;
-	const int size = N*sizeof(float);
-	
-	cudaMalloc( (void**)&cd, size );
-	dim3 dimBlock( blocksize, 1 );
-	dim3 dimGrid( 1, 1 );
-	simple<<<dimGrid, dimBlock>>>(cd);
-	cudaDeviceSynchronize();
-	cudaMemcpy( c, cd, size, cudaMemcpyDeviceToHost ); 
-	cudaFree( cd );
-	
+
+	const int size = N * sizeof(float);
+	dim3 dimBlock(blocksize, 1);
+	dim3 dimGrid(1, 1);
+	// 	float *c = new float[N];
+	// float *cd;
+	// cudaMalloc((void **)&cd, size);
+	// 	simple<<<dimGrid, dimBlock>>>(cd);
+	//	cudaDeviceSynchronize();
+	// 	cudaMemcpy(c, cd, size, cudaMemcpyDeviceToHost);
+	// cudaFree(cd);
+
+	float *s = new float[N];
+	float *sd;
+	cudaMalloc((void **)&sd, size);
+
+	ssqrt<<<dimGrid, dimBlock>>>(sd);
+
+	cudaMemcpy(s, sd, size, cudaMemcpyDeviceToHost);
+
+	cudaFree(sd);
+
+	// for (int i = 0; i < N; i++)
+	// 	printf("%f ", c[i]);
+	// printf("\n");
+	// delete[] c;
+	// printf("done\n");
+
 	for (int i = 0; i < N; i++)
-		printf("%f ", c[i]);
+		printf("%f ", s[i]);
 	printf("\n");
-	delete[] c;
+	delete[] s;
 	printf("done\n");
 	return EXIT_SUCCESS;
 }
