8d5bd7aa9630ed445ef5b66b444baeb295a7bff6
Jillberg <jillordeal@gmail.com>
2023-12-04 22:39:58 +0800

    changes

--- Lab4/matrix_cpu.c
+++ Lab4/matrix_cpu.c
@@ -18,7 +18,7 @@ void add_matrix(float *a, float *b, float *c, int N)
 
 int main()
 {
-	const int N = 16;
+	const int N = 512;
 
 	float a[N*N];
 	float b[N*N];
@@ -30,18 +30,17 @@ int main()
 			a[i+j*N] = 10 + i;
 			b[i+j*N] = (float)j / N;
 		}
-	
-	int start = GetMicroseconds();
-	int end = GetMicroseconds();
-	printf("start: %d end: %d\n",start,end);
+	double start = GetSeconds();
+	double end = GetSeconds();
+	printf("start: %0.5lf end: %0.5lf\n",start,end);
 	add_matrix(a, b, c, N);
-printf("Execution Time: %e seconds\n", ((double)(end - start)) / 1000000.0);
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			printf("%0.2f ", c[i+j*N]);
-		}
-		printf("\n");
-	}
+printf("Execution Time: %0.5lf seconds\n", (end - start) / 1000000.0);
+	// for (int i = 0; i < N; i++)
+	// {
+	// 	for (int j = 0; j < N; j++)
+	// 	{
+	// 		printf("%0.2f ", c[i+j*N]);
+	// 	}
+	// 	printf("\n");
+	// }
 }
