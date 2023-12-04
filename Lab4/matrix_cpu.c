f85931732783eb01fc7b17a8f3607a02f15743f9
Jillberg <jillordeal@gmail.com>
2023-12-04 22:55:39 +0800

    use malloc

--- Lab4/matrix_cpu.c
+++ Lab4/matrix_cpu.c
@@ -1,46 +1,65 @@
-// Matrix addition, CPU version
-// gcc matrix_cpu.c -o matrix_cpu -std=c99
-
 #include <stdio.h>
-#include<milli.h>
+#include <stdlib.h>
+#include <milli.h>
 
 void add_matrix(float *a, float *b, float *c, int N)
 {
-	int index;
-	
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-		{
-			index = i + j*N;
-			c[index] = a[index] + b[index];
-		}
+    int index;
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            index = i + j * N;
+            c[index] = a[index] + b[index];
+        }
 }
 
 int main()
 {
-	const int N = 512;
+    const int N = 1024;
 
-	float a[N*N];
-	float b[N*N];
-	float c[N*N];
+    // Allocate memory for matrices
+    float *a = (float *)malloc(N * N * sizeof(float));
+    float *b = (float *)malloc(N * N * sizeof(float));
+    float *c = (float *)malloc(N * N * sizeof(float));
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-		{
-			a[i+j*N] = 10 + i;
-			b[i+j*N] = (float)j / N;
-		}
-	double start = GetSeconds();
+    // Check if memory allocation is successful
+    if (a == NULL || b == NULL || c == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        return -1;
+    }
 
-	add_matrix(a, b, c, N);
-		double end = GetSeconds();
-printf("Execution Time: %0.6lf seconds\n", (end - start));
-	// for (int i = 0; i < N; i++)
-	// {
-	// 	for (int j = 0; j < N; j++)
-	// 	{
-	// 		printf("%0.2f ", c[i+j*N]);
-	// 	}
-	// 	printf("\n");
-	// }
+    // Initialize matrices
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+        {
+            a[i + j * N] = 10 + i;
+            b[i + j * N] = (float)j / N;
+        }
+
+    double start = GetSeconds();
+    add_matrix(a, b, c, N);
+    double end = GetSeconds();
+
+    printf("Execution Time: %0.6lf seconds\n", (end - start));
+
+    // Uncomment the following code if you want to print the result matrix
+    /*
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%0.2f ", c[i + j * N]);
+        }
+        printf("\n");
+    }
+    */
+
+    // Free allocated memory
+    free(a);
+    free(b);
+    free(c);
+
+    return 0;
 }
